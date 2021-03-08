530cf0e09986b8e8bf254b549ba005f2ddaf373a
willjow <williamjow67@gmail.com>
2021-03-07 20:37:52 -0800

    broken wip for kq

--- scripts/auth.py
+++ scripts/auth.py
@@ -1,5 +1,8 @@
 """Module for parsing authorization details."""
 
+import requests
+import secrets
+
 def parse_file(secret_file):
     """Returns access token for authorizing spotify API requests.
 
@@ -13,6 +16,23 @@ def parse_file(secret_file):
         client_id = sf.readline()
         client_secret = sf.readline()
         if client_id and client_secret:
-            return f"Basic {client_id}:{client_secret}"
+            return client_id, client_secret
     return None
 
+
+def generate_state(nbytes=32):
+    """Generates a state string for OAuth."""
+    return secrets.token_urlsafe(nbytes)
+
+
+def get_auth_code(client_id, redirect_uri='https://localhost:8082'):
+    """Requests an authorization code from spotify."""
+    endpoint = 'https://accounts.spotify.com/authorize'
+    params = {'client_id': client_id,
+              'response_type': 'code',
+              'redirect_uri': redirect_uri,
+              'state': generate_state()}
+    response = requests.get(endpoint, params)
+    json_response = response.json()
+    assert json_response['state'] == params['state']
+    return json_reponse['code']
--- scripts/search.py
+++ scripts/search.py
@@ -2,9 +2,22 @@
 
 import requests
 
-def track_search(artist, title):
+def track_search(artist, title, filters=None):
     """Searches spotify for a track.
 
     Parameters
     ----------
+    artist : str
+        The track artist.
+    title : str
+        The track title.
+    filters : str
+        TODO
     """
+    endpoint = 'https://api.spotify.com/v1/search'
+    params={'q': f'artist:{artist}+track:{title}',
+            'type': 'track'},
+    headers={'Accept': 'application/json',
+             'Content-Type': 'application/json',
+             'Authorization': }
+    response = requests.get(endpoint, params, headers=headers)

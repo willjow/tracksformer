0049eed1ed381de6a3862359e38a0d01eb20a6ac
kathleenqin <qinkathleen@gmail.com>
2021-03-11 21:38:15 -0800

    get_access_token function

--- scripts/auth.py
+++ scripts/auth.py
@@ -2,6 +2,8 @@
 
 import requests
 import secrets
+import webbrowser
+import base64
 
 def parse_file(secret_file):
     """Returns access token for authorizing spotify API requests.
@@ -13,8 +15,8 @@ def parse_file(secret_file):
         the user client ID and the second line should be the client secret.
     """
     with open(secret_file) as sf:
-        client_id = sf.readline()
-        client_secret = sf.readline()
+        client_id = sf.readline()[:-1].split(':')[1]
+        client_secret = sf.readline().split(':')[1]
         if client_id and client_secret:
             return client_id, client_secret
     return None
@@ -32,7 +34,19 @@ def get_auth_code(client_id, redirect_uri='https://localhost:8082'):
               'response_type': 'code',
               'redirect_uri': redirect_uri,
               'state': generate_state()}
-    response = requests.get(endpoint, params)
+    # build url with endpoint and params
+    url = requests.Request('GET', endpoint, params=params).prepare().url
+    #print(url.head)
+    webbrowser.open(url)
+    return None
+
+def get_access_token(auth_code):
+    """Exchange authorization code with access token."""
+    endpoint = 'https://accounts.spotify.com/api/token'
+    params = {'grant_type': 'authorization_code',
+              'code': auth_code,
+              'redirect_uri': 'https://localhost:8082' }
+    header = {'Authorization': 'Basic ' + base64.standard_b64encode("{}:{}".format(client_id, client_secret).encode('UTF-8')).decode('ascii')}
+    response = requests.post(endpoint, params=params, headers=header)
     json_response = response.json()
-    assert json_response['state'] == params['state']
-    return json_reponse['code']
+    return json_response['access_token']
